bf03af4f44990da33c3b21b49e0f5836f51d2765
Patrick Horne <patrickahorne@gmail.com>
2019-12-09 20:02:29 +1100

    Update login.js, put comments in app.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,19 @@
 import React, {useState, useEffect} from 'react';
+import {BACKEND_URL} from '../../config';
 
 function Login(props) {
+  function handleLogin(credentials) {
+    fetch()
+  };
   return (
-    <p>This is the Login Component</p>
+    <>
+      <p>Start Login Component</p>
+      <form>
+        <input placeholder="Email"></input>
+        <input placeholder="Password" type="password"></input>
+        </form>
+      <p>End Login Component</p>
+    </>
   );
 };
 
